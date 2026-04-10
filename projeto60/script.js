8d4c983601da01615f14acbbaa5bf66ae1def397
Gabriel Rocha <111612298+Gabriel-H-Alves@users.noreply.github.com>
2026-04-10 08:46:27 -0300

    Update script.js

--- projeto60/script.js
+++ projeto60/script.js
@@ -1,18 +1,18 @@
-// Selecting DOM elements
+
 const startBtn = document.querySelector("#startBtn"),
   endBtn = document.querySelector("#endBtn"),
   prevNext = document.querySelectorAll(".prevNext"),
   numbers = document.querySelectorAll(".link");
-// Setting an initial step
+
 let currentStep = 0;
-// Function to update the button states
+
 const updateBtn = () => {
-  // If we are at the last step
+
   if (currentStep === 4) {
     endBtn.disabled = true;
     prevNext[1].disabled = true;
   } else if (currentStep === 0) {
-    // If we are at the first step
+  
     startBtn.disabled = true;
     prevNext[0].disabled = true;
   } else {
@@ -22,13 +22,13 @@ const updateBtn = () => {
     prevNext[0].disabled = false;
   }
 };
-// Add event listeners to the number links
+
 numbers.forEach((number, numIndex) => {
   number.addEventListener("click", (e) => {
     e.preventDefault();
-    // Set the current step to the clicked number link
+    
     currentStep = numIndex;
-    // Remove the "active" class from the previously active number link
+   
     document.querySelector(".active").classList.remove("active");
     
     number.classList.add("active");
